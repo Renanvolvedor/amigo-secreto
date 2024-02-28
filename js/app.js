9e105529c41d1d79887ba74ba7aef1612443289c
Renan Cardoso Segretti <renanony@gmail.com>
2024-02-28 11:52:49 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+//Array vazia que vai receber cada amigo.
+let amigos = [];
+//Pega os campos onde digita o nome do amigo e onde vai ser adicionado e onde vai aparecer os amigos sorteados.
+let amigo = document.getElementById('nome-amigo');
+let lista = document.getElementById('lista-amigos');
+let sorteio = document.getElementById('lista-sorteio');
+
+function adicionar() {
+    //Adiciona o amigo na lista, se não for vazio.
+    if (amigo.value == '') {
+        alert('Digite o nome de um amigo.');
+    } else {
+        amigos.push(amigo.value);
+        //Se a lista de amigos estiver vazia, adiciona o amigo.
+        if (lista.textContent == '') {
+            lista.textContent = amigo.value;
+        //Se não, adiciona o que estava antes mais o amigo.
+        } else {
+            lista.textContent = lista.textContent + ', ' + amigo.value;
+        }
+        //Limpa o campo pra adicionar outro amigo.
+        amigo.value = '';
+    }
+}
+
+
+function sortear () {
+    //Embaralha a ordem de amigos adicionados na array.
+    embaralhar(amigos);
+    //Esvazia o sorteio toda vez.
+    sorteio.innerHTML = '';
+    //Para cada vez que o 'i' for menor que a quantidade de elementos da lista, adiciona 1 ao 'i'.
+    for (let i = 0; i < amigos.length; i++) {
+        //Se o i for igual a quantidade de amigos na lista menos 1 (pois começa no 0).
+        //Isso serve para o último elemento da lista também funcionar.
+        if (i == amigos.length - 1) {
+            //Pega o último elemento da lista e o coloca com o primeiro.
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br/>';
+        } else {
+            //Se não pega o elemento atual da lista e o coloca com o próximo.
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br/>';
+        }
+    }
+}
+
+function reiniciar() {
+    amigo.value = '';
+    lista.textContent = '';
+    sorteio.innerHTML = '';
+    amigos = [];
+
+}
+
+//Função pronta tirada da internet que embaralha os elementos de uma array.
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
